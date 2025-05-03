c73718416acd8138d92f2702ce2024eec903faff
@Kernal_Knight🗡️🛡️💻 <143244039+KernalKnight@users.noreply.github.com>
2025-05-03 16:45:18 +0530

    Delete Hello_World directory

--- Hello_World/04_Initials.py
+++
@@ -1,9 +0,0 @@
-# Fun fact: I love programming
-
-print("RRRRR    L    ")
-print("R    R   L    ")
-print("R    R   L    ")
-print("RRRRRR   L    ")
-print("R   R    L    ")
-print("R    R   L    ")
-print("R     R  LLLLLL")
--- Hello_World/5_Snail_Mail.py
+++
@@ -1,5 +0,0 @@
-print("5/3/2025") # Current date
-print("Feeling energetic ⚡") # How am I feeling right now
-print("Coding my own complex projects ⌨️") # Accomplishing by coding
-print("Keep coding 😊") # A little message
-print("💻") # Fav emoji
